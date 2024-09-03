448605827b0a6d7957a9a109f4b4678ba33f36a1
vivekaviva <vivekaloganathan37@gmail.com>
2024-09-03 11:22:50 +0530

    Multiple File upload

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ let uploadsss = multer({
   },
   fileFilter: function (req, file, cb) {
     console.log("File details:", file);
-    let filetypes = /jpeg|jpg/;
+    let filetypes = /jpeg|jpg|png/;
     let mimetype = filetypes.test(file.mimetype);
     let extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     console.log("MIME type valid:", mimetype);
@@ -48,8 +48,54 @@ let uploadsss = multer({
   },
 }).single("uploadMyPic");
 
+let uploadsMultiple = multer({
+  storage: storages,
+  limits: {
+    fileSize: maxSize,
+  },
+  fileFilter: function (req, file, cb) {
+    console.log("File details:", file);
+    let filetypes = /jpeg|jpg|png/;
+    let mimetype = filetypes.test(file.mimetype);
+    let extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    console.log("MIME type valid:", mimetype);
+    console.log("Extension valid:", extname);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+
+    // Pass an error instance to the callback
+    return cb(new Error("File format is not supported"), false);
+  },
+}).array("uploadMultiplePic", 3);
+
 app.get("/", (req, res) => {
   res.send("Welcome Page");
+});
+
+app.get("/multipleUpload", (req, res) => {
+  res.render("fileUploadMultiple");
+});
+
+app.post("/uploadedMultiple", (req, res) => {
+  console.log("Upload endpoint hit");
+  uploadsMultiple(req, res, function (err) {
+    if (err) {
+      if (err instanceof multer.MulterError) {
+        if (err.code === "LIMIT_FILE_SIZE") {
+          return res
+            .status(400)
+            .send("Error: File size exceeds the maximum limit of 2MB");
+        }
+      } else if (err) {
+        console.log("Error:", err.message);
+        return res.status(500).send(`Error: ${err.message}`);
+      }
+    } else {
+      res.send("Success. Image Uploaded!");
+    }
+  });
 });
 
 app.post("/uploaded", (req, res) => {
